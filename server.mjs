73c9a6473ef723c35f7f1db7747993f7a4ee8ded
HadiRaza04 <hadiraza.edu@gmail.com>
2022-11-25 13:51:12 +0500

    my server code

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    console.log("request ip: ", req.ip);
+
+  res.send('Hello World! ' + new Date().toString() + "Hello World Server!" );
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
