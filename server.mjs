bb84bcff41a29ebb2ba52aa8dcf4839ba707cf92
HadiRaza04 <hadiraza.edu@gmail.com>
2022-11-26 20:13:31 +0500

    single page success

--- server.mjs
+++ server.mjs
@@ -1,13 +1,31 @@
 import express from 'express';
-const app = express()
-const port = process.env.PORT || 3000
+import path from 'path';
 
-app.get('/', (req, res) => {
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/abc', (req, res) => {
 
     console.log("request ip: ", req.ip);
-
   res.send('Hello World! ' + new Date().toString() + "Hello World Server!" );
 })
+
+app.get('/getweather', (req, res) => {
+
+  console.log("request ip: ", req.ip);
+res.send('Hello World! ' + new Date().toString() + "Hello World Server!" );
+})
+
+app.get('/gettime', (req, res) => {
+
+  console.log("request ip: ", req.ip);
+res.send('Hello World! ' + new Date().toString() + "Hello World Server!" );
+})
+
+const __divname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
